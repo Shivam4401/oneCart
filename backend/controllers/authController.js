5cd6dcb5ddbca11b853046a0db7dde52265653ef
Shivam kumar <146973981+Shivam4401@users.noreply.github.com>
2025-07-27 22:54:53 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -34,8 +34,8 @@ export const register = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "strict",
-            secure: false,
+            sameSite: "none",
+            secure: true,
         });
         //send response for user creation
         return res.status(200).json({
@@ -70,8 +70,8 @@ export const login =async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "strict",
-            secure: false,
+            sameSite: "none",
+            secure: true,
         });
         
         res.status(200).json({
@@ -112,8 +112,8 @@ export const googleLogin = async (req,res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "strict",
-            secure: false,
+            sameSite: "none",
+            secure: true,
         });
         
         res.status(200).json({
@@ -136,8 +136,8 @@ export const adminLogin = async (req, res) => {
             res.cookie("token", token, {
                 httpOnly: true,
                 maxAge: 1 * 24 * 60 * 60 * 1000,
-                sameSite: "strict",
-                secure: false,
+                sameSite: "none",
+                secure: true,
             });
             
             res.status(200).json({
